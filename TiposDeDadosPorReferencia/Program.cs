05780528e92e814ea24cef405e6bcb3a7fd6b575
antoni025 <antonio587@ymail.com>
2024-07-31 11:46:05 -0300

    Add files via upload

---
+++ TiposDeDadosPorReferencia/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TiposDeDadosPorReferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Tipo de dados por referencia
+            string texto = "Essa é uma mensagem";
+            Console.WriteLine(texto);
+
+            //dados object
+            object valor_inteiro = 123;
+            object valor_real = 123.321;
+            object valor_texto = "esse é um texto";
+            object valor_caracter = 'v';
+            object valor_logico = true;
+        }
+    }
+}
